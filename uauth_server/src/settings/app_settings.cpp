5270c65533d75426e3820e949254697c1abc13d3
yarik111475 <yarik111475@mail.ru>
2023-07-28 20:21:14 +0400

    -temporary corrections

--- uauth_server/src/settings/app_settings.cpp
+++ uauth_server/src/settings/app_settings.cpp
@@ -1,6 +1,7 @@
 #include "app_settings.h"
 
 #include <cstdlib>
+#include "spdlog/spdlog.h"
 
 app_settings::app_settings(std::string etc_uauth_dir, std::shared_ptr<spdlog::logger> logger_ptr)
     :etc_uauth_dir_{etc_uauth_dir},logger_ptr_{logger_ptr}
@@ -18,7 +19,6 @@ bool app_settings::settings_init()
     params_.emplace("db_host","dev3.u-system.tech");
     params_.emplace("db_port","5436");
     params_.emplace("db_name","u-auth");
-    return true;
 
     UA_DB_DSN_=std::getenv("UA_DB_DSN");
     UA_DB_POOL_SIZE_MIN_=std::getenv("UA_DB_POOL_SIZE_MIN");
@@ -34,6 +34,34 @@ bool app_settings::settings_init()
 
     UA_SENTRY_DSN_=std::getenv("UA_SENTRY_DSN");
     UA_SENTRY_TRACES_SAMPLE_RATE_=std::getenv("UA_SENTRY_TRACES_SAMPLE_RATE");
+
+    params_.emplace("UA_DB_DSN",UA_DB_DSN_);
+    params_.emplace("UA_DB_POOL_SIZE_MIN",UA_DB_POOL_SIZE_MIN_);
+    params_.emplace("UA_DB_POOL_SIZE_MAX",UA_DB_POOL_SIZE_MAX_);
+    params_.emplace("UA_LOG_LEVEL",UA_LOG_LEVEL_);
+
+    params_.emplace("UA_ORIGINS",UA_ORIGINS_);
+    params_.emplace("UA_SSL_WEB_CRT_VALID",UA_SSL_WEB_CRT_VALID_);
+    params_.emplace("UA_CA_CRT_PATH",UA_CA_CRT_PATH_);
+    params_.emplace("UA_SIGNING_CA_CRT_PATH",UA_SIGNING_CA_CRT_PATH_);
+    params_.emplace("UA_SIGNING_CA_KEY_PATH",UA_SIGNING_CA_KEY_PATH_);
+    params_.emplace("UA_SIGNING_CA_KEY_PASS",UA_SIGNING_CA_KEY_PASS_);
+
+    auto it {params_.begin()};
+    while(it!=params_.end()){
+        if(it->value().is_null()){
+            if(logger_ptr_){
+                logger_ptr_->critical("{}, env variable {} has empty value!",
+                    BOOST_CURRENT_FUNCTION,std::string {it->key()});
+            }
+            return false;
+        }
+        ++it;
+    }
+
+    params_.emplace("UA_SENTRY_DSN",UA_SENTRY_DSN_);
+    params_.emplace("UA_SENTRY_TRACES_SAMPLE_RATE",UA_SENTRY_TRACES_SAMPLE_RATE_);
+    return true;
 }
 
 void app_settings::value_set(const std::string &key, const std::string &value)
