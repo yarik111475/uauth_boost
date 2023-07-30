b844d38a50fccf05d3b08875c72a80f13733b9fc
yarik111475 <yarik111475@mail.ru>
2023-07-30 15:30:28 +0400

    -add x509 methods prototypes

--- uauth_server/src/x509/x509_generator.cpp
+++ uauth_server/src/x509/x509_generator.cpp
@@ -1,6 +1,16 @@
 #include "x509_generator.h"
 
-x509_generator::x509_generator()
-{
+x509_generator::x509_generator(const boost::json::object &params, std::shared_ptr<spdlog::logger> logger_ptr)
+    :params_{params},logger_ptr_{logger_ptr}
+{    
+}
 
+bool x509_generator::sign_agent_crt(const std::vector<char> &csr_buffer, std::vector<char> &crt_buffer)
+{
+    return true;
+}
+
+bool x509_generator::create_user_crt(const std::string &user_uid, const std::string &crt_pass, std::vector<char> &crt_buffer)
+{
+    return true;
 }
--- uauth_server/src/x509/x509_generator.h
+++ uauth_server/src/x509/x509_generator.h
@@ -1,11 +1,26 @@
 #ifndef X509_GENERATOR_H
 #define X509_GENERATOR_H
 
+#include <string>
+#include <vector>
+#include <memory>
+#include <boost/json.hpp>
+
+namespace spdlog{
+    class logger;
+}
 
 class x509_generator
 {
+private:
+    boost::json::object params_ {};
+    std::shared_ptr<spdlog::logger> logger_ptr_ {nullptr};
 public:
-    x509_generator();
+    explicit x509_generator(const boost::json::object& params,std::shared_ptr<spdlog::logger> logger_ptr);
+    ~x509_generator()=default;
+
+    bool sign_agent_crt(const std::vector<char>& csr_buffer,std::vector<char>& crt_buffer);
+    bool create_user_crt(const std::string& user_uid,const std::string& crt_pass,std::vector<char>& crt_buffer);
 };
 
 #endif // X509_GENERATOR_H
