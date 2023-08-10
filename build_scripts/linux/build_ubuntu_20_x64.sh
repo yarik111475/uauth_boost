c7017978ebebc3a8bf86abd9cc1a0ca24cba4d41
Антон Скробот <skant9372@gmail.com>
2023-08-10 18:19:07 +0300

    Add gitlab-ci and Dockerfile
    Edit ubuntu build script

--- build_scripts/linux/build_ubuntu_20_x64.sh
+++ build_scripts/linux/build_ubuntu_20_x64.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 set -e
 
-USER_NAME=$(whoami)
 CURRENT_DIR=$(pwd)
 
-echo "USER_NAME: ${USER_NAME}"
 echo "CURRENT_DIR: ${CURRENT_DIR}"
 echo "Create needed directories"
 
-mkdir -p "${CURRENT_DIR}"/build
-BUILD_DIR="${CURRENT_DIR}"/build
-OPENSSL_LIB_DIR="/usr/lib/x86_64-linux-gnu"
-POSTGRE_LIB_DIR="/usr/lib/x86_64-linux-gnu"
+BUILD_DIR=${CURRENT_DIR}/build
+rm -rf ${BUILD_DIR} && mkdir -p ${BUILD_DIR}
 
-HOME_DIR="/home/${USER_NAME}"
-INSTALL_DIR="${HOME_DIR}"/uauth
-rm -rf ${INSTALL_DIR}
-mkdir -p "${HOME_DIR}"/uauth
+INSTALL_DIR=${U_AUTH_DIR}
+rm -rf ${INSTALL_DIR} && mkdir -p ${INSTALL_DIR}
 
 #cmake options
 CMAKE_PATH="/usr/bin/cmake"
 CMAKE_MAKE_PROGRAM="/usr/bin/ninja"
 
 #run cmake
-cd "${BUILD_DIR}"
-${CMAKE_PATH}  ../../.. `
+cd ${BUILD_DIR}
+VERSION=${VERSION} ${CMAKE_PATH}  ../../.. `
 ` -DCMAKE_MAKE_PROGRAM=$CMAKE_MAKE_PROGRAM`
 ` -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR`
 ` -GNinja`
@@ -34,10 +28,14 @@ ${CMAKE_PATH}  ../../.. `
 
 #build and install targets
 ${CMAKE_PATH}  --build . --parallel --target all install
-rm -rf "${BUILD_DIR}"
+rm -rf ${BUILD_DIR}
 
-mkdir -p ${INSTALL_DIR}/lib
+
 LIB_DIR=${INSTALL_DIR}/lib
+mkdir -p ${LIB_DIR}
+
+OPENSSL_LIB_DIR="/usr/lib/x86_64-linux-gnu"
+POSTGRE_LIB_DIR="/usr/lib/x86_64-linux-gnu"
 
 cp -f ${POSTGRE_LIB_DIR}/libpq.so.5.12 ${LIB_DIR}/libpq.so.5.12
 cp -f ${OPENSSL_LIB_DIR}/libssl.so.1.1    ${LIB_DIR}/libssl.so.1.1
